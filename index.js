e9ed395e9aefa271aafd85f261fa111e69418ace
chima1707 <xhimat123@gmail.com>
2018-03-18 17:09:58 +0100

    removed assignment

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ module.exports = function formatElapsedTime (elapsedMilliseconds) {
   if (typeof elapsedMilliseconds !== 'number') {
     return '00:00:00'
   }
-  elapsedMilliseconds = Math.abs(elapsedMilliseconds)
+  const elapsedTime = Math.abs(elapsedMilliseconds)
   let hours = 0
-  let seconds = ((elapsedMilliseconds / 1000) % 60).toFixed(2)
+  let seconds = ((elapsedTime / 1000) % 60).toFixed(2)
   if (+seconds < 10) {
     seconds = '0' + seconds
   }
-  let minutes = Math.floor((elapsedMilliseconds / 1000) / 60)
+  let minutes = Math.floor((elapsedTime / 1000) / 60)
   if (minutes > 59) {
     hours = Math.floor((minutes / 60))
     minutes = minutes % 60
